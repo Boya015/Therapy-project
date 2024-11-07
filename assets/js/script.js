8dfd9a64accd8a1427bba07e0279672e89b19321
Johann <johanncareer@gmail.com>
2024-11-07 21:27:56 +0700

    Added mobile nav, updated some responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-function toggleMenu() {
-    const navMenu = document.querySelector('.nav-menu');
-    navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
-  }
+function toggleMobileMenu() {
+  const navMenuMobile = document.getElementById("navMenuMobile");
+  navMenuMobile.classList.toggle("nav-menu-active");
+}
+
+document.querySelectorAll('.nav-menu-mobile a').forEach(link => {
+  link.addEventListener('click', () => {
+    document.getElementById("navMenuMobile").classList.remove("nav-menu-active");
+  });
+});
