04e532013e374480b8489b6632e7b12c967eda76
Johann <johanncareer@gmail.com>
2025-02-10 12:39:44 +0700

    Added contact form

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+// SMTP Configuration
+define('SMTP_HOST', 'smtp.gmail.com'); // Your website's SMTP host (e.g., mail.yourwebsite.com) my email is Google Workspace so I use smtp.gmail.com
+define('SMTP_PORT', 587); // 465 for SSL, 587 for TLS
+define('SMTP_USERNAME', 'johann@toris.co.za'); // Use an email from your domain
+define('SMTP_PASSWORD', 'your_password'); // Email password
+
+// Sender details (MUST be from the website's domain)
+define('EMAIL_FROM', 'johann@toris.co.za'); 
+define('EMAIL_FROM_NAME', 'Therapy Website Contact');
+
+// Recipient details (Must be the website email as well)
+define('EMAIL_TO', 'johanncareer@gmail.com');
+define('EMAIL_TO_NAME', 'Therapy Team');
+
+// Email settings
+define('EMAIL_SUBJECT', 'New Contact Form Submission');
+?>
